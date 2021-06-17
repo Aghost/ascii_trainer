696355ede5cc48dde07e200393762f22ead966ff
Your Name <you@example.com>
2021-06-18 00:54:42 +0200

    refactor

--- ascii_trainer.rb
+++ ascii_trainer.rb
@@ -6,34 +6,32 @@ class Question
     @answers = [ position, rand(32..127), rand(32..127), rand(32..127), rand(32..127) ].uniq.shuffle
   end
 
-  def info
-    @answers.each { |a| puts a }
-  end
-
   def ask
     system("clear")
-    puts "which is #{@position}?"
 
     i = 0
     @answers.each{ |a| puts "#{i += 1} #{a.chr}" }
 
-    puts "(char @ #{@position} = (#{@position.chr}))"
+    print "Which is at: #{@position}? "
     answer = gets.chomp
 
     if answer == @position.chr || @answers[(answer).to_i - 1].chr == @position.chr
       system("clear")
       puts "\nyay! that is correct!\n"
-      gets
+    else
+      puts "\nfalse! the answer was: ( #{@position.chr} ), not ( #{@answers[answer.to_i - 1].chr} ) \n"
     end
 
+    gets
   end
+
 end
 
-nums = (32..128).each { |num|
+nums = (32..128).each do |num|
   answer = num.chr
   false_answers = [num,0,0,0]
   (1..3).each{ |position| false_answers[position] = rand(32..127) }
-}
+end
 
 questions = (32..127).map{ |n| Question.new(n) }
 questions.shuffle.each { |q| q.ask}
